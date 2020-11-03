82bf0b493b4f24ef474a685b66abcf42bc6dc5c9
b0101520154 <71687764+b0101520154@users.noreply.github.com>
2020-11-03 16:06:01 +0800

    新增傳送門控制

---
+++ Unity2Dame/Assets/Scripts/MenuManger.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManger : MonoBehaviour
+{    
+    // 註解:說明
+    //按鈕如何跟程式溝通
+    //需要:方法(函數、功能) - 一堆有功能的程式，演算法
+    //語法
+    //修飾詞 類型 方法名稱() {}
+    //私人 private(預設) - 不可被按鈕呼叫
+    //公開 pubic - 可以被按鈕呼叫
+    //void 無類型
+    public void StartGame()
+    {
+        print("開始遊戲");          //輸出("訊息") - 文字要加雙引號，儀表板Console
+
+        SceneManager.LoadScene("關卡1");   //場景管理.載入場景("名稱")
+     //或用SceneManager.LoadScene(1);   //場景管理.載入場景(編號)
+    }
+    public void QuitGame()
+    {
+        print("結束遊戲");
+        Application.Quit();
+    }
+}
---
+++ Unity2Dame/Assets/Scripts/player.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    //欄位:儲存資料
+    //語法:
+    //修飾詞 類型 名稱 (指定 值)；
+    //None 空值:沒東西 - 程式錯誤
+    public GameObject final;
+
+    //碰撞偵測條件
+    //1.兩個物件必須有碰撞器 Collider2D
+    //2.兩者必須有至少一個剛體 Rigidbody2D
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        print("碰到傳送門了");
+        final.SetActive(true);       //結束畫面.啟動設定(是) - 顯示
+    }
+
+}
