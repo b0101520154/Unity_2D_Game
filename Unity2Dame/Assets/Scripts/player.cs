52705a3fffa56bc7ff2968373724b965431e0269
b0101520154 <71687764+b0101520154@users.noreply.github.com>
2020-11-10 16:02:59 +0800

    2D完成

--- Unity2Dame/Assets/Scripts/player.cs
+++ Unity2Dame/Assets/Scripts/player.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;   //引用 介面 API
 
 public class player : MonoBehaviour
 {
@@ -7,14 +8,36 @@ public class player : MonoBehaviour
     //修飾詞 類型 名稱 (指定 值)；
     //None 空值:沒東西 - 程式錯誤
     public GameObject final;
+    //道具數量
+    public Text textCount;
+    //數量
+    public int count;
 
     //碰撞偵測條件
     //1.兩個物件必須有碰撞器 Collider2D
     //2.兩者必須有至少一個剛體 Rigidbody2D
+    //collision 儲存碰到物件的資訊
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        print("碰到傳送門了");
-        final.SetActive(true);       //結束畫面.啟動設定(是) - 顯示
+        //判斷式if語法:
+        //if(條件) {條件成立時會執行的程式方塊}
+        //如果碰到物件的名稱是傳送門就...
+        if(collision.name == "傳送門")
+        {
+            final.SetActive(true);      //結束畫面.啟動設定(是) - 顯示
+        }    
+        
+        //如果碰到物件的標籤是鑽石就吃掉它
+        if (collision.tag == "櫻桃") ;
+        {
+            //刪除(碰到的遊戲物件)
+            //gameObject此腳本的遊戲物件
+            Destroy(collision.gameObject);
+            //遞增
+            count++;
+            //道具數量的文字= "鑽石數量 : " + 數量
+            textCount.text = "鑽石數量 : " + count;
+        }
     }
 
 }
